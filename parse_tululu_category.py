ce4deb37781031b828c43e97e6e44c9fa5af96e9
Max Korob <korobejnikovmax@gmail.com>
2021-07-15 22:33:09 +0500

    Added parse_tululu_category

---
+++ parse_tululu_category.py
@@ -0,0 +1,31 @@
+from contextlib import suppress
+from urllib.parse import urljoin
+
+from bs4 import BeautifulSoup
+from urllib3 import disable_warnings
+from urllib3.exceptions import InsecureRequestWarning
+
+from run_downloader import raise_if_redirect, get_page
+
+
+def parse_category_page(response):
+    soup: BeautifulSoup = BeautifulSoup(response.text, 'lxml')
+    with suppress(Exception):
+        books_urls = [
+            urljoin(response.request.url, book_item.find('a')['href'])
+            for book_item in soup.find_all('div', class_='bookimage')
+        ]
+
+        return books_urls
+
+
+def download_tululu_by_category(url):
+    response = get_page(url)
+    books_urls = parse_category_page(response)
+    print(books_urls)
+
+
+if __name__ == '__main__':
+    disable_warnings(InsecureRequestWarning)
+    category_url = 'https://tululu.org/l55/'
+    download_tululu_by_category(category_url)
